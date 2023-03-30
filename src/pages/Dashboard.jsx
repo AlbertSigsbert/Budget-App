bda5c73ead3076d7d3715752f5adde3d8b88f67a
Albert <ishengomaalbert@gmail.com>
2023-03-30 12:09:59 +0300

    Custom Error Page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,28 +16,23 @@ export function dashboardLoader() {
 
 //action
 
-export async function dashboardAction({request}){
+export async function dashboardAction({ request }) {
   const data = await request.formData();
-  
+
   const formData = Object.fromEntries(data);
 
   try {
-    localStorage.setItem("userName", JSON.stringify(formData.userName));
-     return toast.success(`Welcome, ${formData.userName}`)
+    throw new Error('Error')
+    // localStorage.setItem("userName", JSON.stringify(formData.userName));
+    // return toast.success(`Welcome, ${formData.userName}`);
   } catch (e) {
-    throw new Error("There was a problem creating your account.")
+    throw new Error("There was a problem creating your account.");
   }
-
- 
 }
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <>
-    {userName ? <p>{userName}</p> : <Intro/>}
-    </>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 
 export default Dashboard;
